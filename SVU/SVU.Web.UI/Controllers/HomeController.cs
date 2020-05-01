3d81c14fd5f79e27125a3bd98012352506a54e48
Saria Houloubi <houloubis@gmail.com>
2020-05-01 15:15:10 +0400

    Removed all new project added files

--- SVU/SVU.Web.UI/Controllers/HomeController.cs
+++ SVU/SVU.Web.UI/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using SVU.Web.UI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SVU.Web.UI.Controllers
 {
@@ -13,17 +7,6 @@ namespace SVU.Web.UI.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- SVU/SVU.Web.UI/Startup.cs
+++ SVU/SVU.Web.UI/Startup.cs
@@ -24,13 +24,6 @@ namespace SVU.Web.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -50,8 +43,9 @@ namespace SVU.Web.UI
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+
 
             app.UseMvc(routes =>
             {
