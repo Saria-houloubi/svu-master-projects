953dd224edc7299e729e0236c872544a316f8293
Saria Houloubi <houloubis@gmail.com>
2020-05-16 16:45:43 +0400

    Done bayes

--- SVU/SVU.Web.UI/Controllers/HomeworkController.cs
+++ SVU/SVU.Web.UI/Controllers/HomeworkController.cs
@@ -23,6 +23,12 @@ namespace SVU.Web.UI.Controllers
         public IDataSetDatabaseService DataSetDatabaseService { get; private set; }
         public ILoggingService LoggingService { get; private set; }
         public IMemoryCache MemoryCache { get; private set; }
+
+        /// <summary>
+        /// Constent keys
+        /// </summary>
+        public static readonly string AVG_KEY = "Average";
+        public static readonly string STD_KEY = "StandardDeviation";
         #endregion
 
         #region Constructer
@@ -88,101 +94,150 @@ namespace SVU.Web.UI.Controllers
                     //      dic => columnNam => (result,errorRate)
                     //NOTE: All the calcuations are done in the controller side just for easy look 
                     //  a better approach would be taking them into a service side interface
-                    var calcuationsCache = await MemoryCache.GetOrCreate(string.Join('-', "Bayes", model.Target, model.IgnoreProperties), async options =>
-                     {
-                         //Get the records from the db
-                         var dbSet = await DataSetDatabaseService.GetDbSet(model.DbSet);
-                         var calcuations = new Dictionary<string, Dictionary<string, float>>
-                         {
+                    var calcuationsCache = await MemoryCache.GetOrCreate(string.Join('-', "Bayes", model.DbSet, model.Target, model.IgnoreProperties), async options =>
+                      {
+                          //Get the records from the db
+                          var dbSet = await DataSetDatabaseService.GetDbSet(model.DbSet);
+                          var calcuations = new Dictionary<string, Dictionary<string, float>>
+                          {
                             //Add the target as it is a spical value
                             { model.Target, new Dictionary<string, float>() }
-                         };
-                         try
-                         {
-                             //try to get the target values based on the name of the property that is provided in the target
-                             var targetValues = dbSet.Select(item => item.GetType().GetProperty(model.Target).GetValue(item).ToString());
+                          };
+                          try
+                          {
+                              //try to get the target values based on the name of the property that is provided in the target
+                              var targetValues = dbSet.Select(item => item.GetType().GetProperty(model.Target).GetValue(item).ToString());
 
-                             //Get the distinct values
-                             var targetDistinctValues = targetValues.Distinct();
+                              //Get the distinct values
+                              var targetDistinctValues = targetValues.Distinct();
 
-                             //Get the count of each occurrence of the target value
-                             var targetCount = new Dictionary<string, int>();
-                             foreach (var target in targetDistinctValues)
-                             {
-                                 if (!targetCount.ContainsKey(target.ToString()))
-                                 {
-                                     targetCount.Add(target.ToString(), targetValues.Where(t => t.ToString() == target.ToString()).Count());
-                                 }
-                                 if (!calcuations[model.Target].ContainsKey(target))
-                                 {
-                                     calcuations[model.Target].Add(target, targetCount[target] / (float)targetValues.Count());
-                                 }
-                             }
-                             //Loop throught the items
-                             foreach (var item in dbSet)
-                             {
-                                 //Get there properties
-                                 foreach (var property in item.GetType().GetProperties())
-                                 {
-                                     //Do not do any calcuations on any ignored properties
-                                     if (!model.IgnoreProperties.Contains(property.Name))
-                                     {
-                                         //Check if the property is a string value or an int due to change of calcualtions
-                                         if (property.PropertyType == typeof(string))
-                                         {
-                                             //Get the value of the property from teh main object
-                                             var propValue = property.GetValue(item).ToString();
+                              //Get the count of each occurrence of the target value
+                              var targetCount = new Dictionary<string, int>();
+                              foreach (var target in targetDistinctValues)
+                              {
+                                  if (!targetCount.ContainsKey(target.ToString()))
+                                  {
+                                      targetCount.Add(target.ToString(), targetValues.Where(t => t.ToString() == target.ToString()).Count());
+                                  }
+                                  if (!calcuations[model.Target].ContainsKey(target))
+                                  {
+                                      calcuations[model.Target].Add(target, targetCount[target] / (float)targetValues.Count());
+                                  }
+                              }
+                              //Loop throught the items
+                              foreach (var item in dbSet)
+                              {
+                                  //Get there properties
+                                  foreach (var property in item.GetType().GetProperties())
+                                  {
+                                      //Do not do any calcuations on any ignored properties
+                                      if (!model.IgnoreProperties.Contains(property.Name))
+                                      {
+                                          //Check if the property is a string value or an int due to change of calcualtions
+                                          if (property.PropertyType == typeof(string))
+                                          {
+                                              //Get the value of the property from teh main object
+                                              var propValue = property.GetValue(item).ToString();
 
-                                             //Get the calcuations for each target
-                                             foreach (var target in targetDistinctValues)
-                                             {
-                                                 var keyName = $"{property.Name}-{propValue}";
-                                                 //Check if we did the calcualtions for that value
-                                                 if (!calcuations.ContainsKey(keyName))
-                                                 {
-                                                     Console.WriteLine(keyName);
-                                                     //Add the new item
-                                                     calcuations.Add(keyName, new Dictionary<string, float>());
-                                                 }
-                                                 //Check if we did the calcuations on that target
-                                                 if (!calcuations[keyName].ContainsKey(target))
-                                                 {
-                                                     //Do the calculations
-                                                     calcuations[keyName].Add(target,
-                                                            (dbSet.Where(obj =>
-                                                            obj.GetType().GetProperty(model.Target).GetValue(obj).ToString() == target &&
-                                                           obj.GetType().GetProperty(property.Name).GetValue(obj).ToString() == propValue
-                                                          ).Count() + 0) / (float)targetCount[target]);
-                                                 }
-                                             }
-                                         }
-                                         //Else we need to find the avg and the STD (standard deviation)
-                                         else
-                                         {
-                                             throw new NotImplementedException();
-                                         }
-                                     }
-                                 }
-                             }
-                         }
-                         catch (System.Exception ex)
-                         {
-                             LoggingService.LogException(ex);
-                             //Clear it right away
-                             options.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMilliseconds(1);
-                             return null;
-                         }
-                         //Keep it in cache for one week only
-                         options.AbsoluteExpiration = DateTimeOffset.UtcNow.AddDays(7);
-                         return calcuations;
-                     });
+                                              //Get the calcuations for each target
+                                              foreach (var target in targetDistinctValues)
+                                              {
+                                                  var keyName = $"{property.Name}-{propValue}";
+                                                  //Check if we did the calcualtions for that value
+                                                  if (!calcuations.ContainsKey(keyName))
+                                                  {
+                                                      //Add the new item
+                                                      calcuations.Add(keyName, new Dictionary<string, float>());
+                                                  }
+                                                  if (!calcuations[keyName].ContainsKey(target))
+                                                  {
+                                                      //Do the calculations
+                                                      calcuations[keyName].Add(target,
+                                                          (dbSet.Where(obj =>
+                                                          obj.GetType().GetProperty(model.Target).GetValue(obj).ToString() == target &&
+                                                         obj.GetType().GetProperty(property.Name).GetValue(obj).ToString() == propValue
+                                                        ).Count() + 1) / (float)targetCount[target]);
+
+                                                  }
+                                              }
+                                          }
+                                          //Else we need to find the avg and the STD (standard deviation)
+                                          else
+                                          {
+                                              //Get the number value of hte property
+                                              if (float.TryParse(property.GetValue(item).ToString(), out float propValue))
+                                              {
+                                                  //Get the calcuations for each target
+                                                  foreach (var target in targetDistinctValues)
+                                                  {
+                                                      var keyName = $"{property.Name}-{target}";
+                                                      //Check if we did the calcualtions for that value
+                                                      if (!calcuations.ContainsKey(keyName))
+                                                      {
+                                                          Console.WriteLine(keyName);
+                                                          //Add the new item
+                                                          calcuations.Add(keyName, new Dictionary<string, float>());
+                                                      }
+
+                                                      if (!calcuations[keyName].ContainsKey(AVG_KEY) && !calcuations[keyName].ContainsKey(STD_KEY))
+                                                      {
+
+                                                          //Get the points where the target is met
+                                                          var points = dbSet.Where(obj =>
+                                                       obj.GetType().GetProperty(model.Target).GetValue(obj).ToString() == target
+                                                      ).Select(obj => obj.GetType().GetProperty(property.Name).GetValue(obj).ToString());
+                                                          //The final results that we need
+                                                          double mean = 0;
+                                                          double std = 0;
+                                                          //Loop through the points and get there number value
+                                                          foreach (var point in points)
+                                                          {
+                                                              if (float.TryParse(point, out float pointValue))
+                                                              {
+                                                                  mean += pointValue;
+                                                              }
+                                                          }
+
+                                                          //get the avg
+                                                          mean /= points.Count();
+                                                          //calcualte the std
+                                                          foreach (var point in points)
+                                                          {
+                                                              if (float.TryParse(point, out float pointValue))
+                                                              {
+                                                                  std += Math.Pow(pointValue - mean, 2);
+                                                              }
+                                                          }
+                                                          std = Math.Sqrt(std / (points.Count() - 1));
+
+                                                          calcuations[keyName].Add(AVG_KEY, (float)mean);
+                                                          calcuations[keyName].Add(STD_KEY, (float)std);
+                                                      }
+                                                  }
+                                              }
+                                          }
+                                      }
+                                  }
+                              }
+                          }
+                          catch (System.Exception ex)
+                          {
+                              LoggingService.LogException(ex);
+                              //Clear it right away
+                              options.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMilliseconds(1);
+                              return null;
+                          }
+                          //Keep it in cache for one week only
+                          options.AbsoluteExpiration = DateTimeOffset.UtcNow.AddDays(7);
+                          return calcuations;
+                      });
 
                     if (calcuationsCache == null)
                     {
                         return InternalServerError();
                     }
                     //Now go throught the properties of the testing example
-                    var results = new Dictionary<string, float>();
+                    var results = new Dictionary<string, double>();
                     foreach (var property in model.TestExample.Properties())
                     {
                         //Check if the property is not form the ignored ones
@@ -195,12 +250,22 @@ namespace SVU.Web.UI.Controllers
                                     //Add the starting result value and defualt to the ratio for each target value
                                     results.Add(target.Key, target.Value);
                                 }
-
-                                if (calcuationsCache.TryGetValue($"{property.Name}-{property.Value}", out Dictionary<string, float> targetResult))
+                                //Check if the value is an number
+                                if (float.TryParse(property.Value.ToString(), out float propIntValue))
                                 {
-                                    if (targetResult.TryGetValue(target.Key, out float probability))
+                                    if (calcuationsCache.TryGetValue($"{property.Name}-{target.Key}", out Dictionary<string, float> targetResult))
                                     {
-                                        results[target.Key] *= probability;
+                                        results[target.Key] *= CalculateNormalDistribution(targetResult[AVG_KEY], targetResult[STD_KEY], propIntValue);
+                                    }
+                                }
+                                else
+                                {
+                                    if (calcuationsCache.TryGetValue($"{property.Name}-{property.Value}", out Dictionary<string, float> targetResult))
+                                    {
+                                        if (targetResult.TryGetValue(target.Key, out float probability))
+                                        {
+                                            results[target.Key] *= probability;
+                                        }
                                     }
                                 }
                             }
@@ -257,5 +322,21 @@ namespace SVU.Web.UI.Controllers
 
         #endregion
 
+
+        #region Helpers
+
+
+        /// <summary>
+        /// Calcuialtes the normal distribution of a point
+        /// </summary>
+        /// <param name="avg"></param>
+        /// <param name="std"></param>
+        /// <param name="value">the value to calculate the distribution for</param>
+        /// <returns></returns>
+        private double CalculateNormalDistribution(float avg, float std, float value)
+        {
+            return (1 / (Math.Sqrt(2 * Math.PI) * std) * Math.Exp(-(Math.Pow((value - avg), 2)) / (2 * Math.Pow(std, 2))));
+        }
+        #endregion
     }
 }
