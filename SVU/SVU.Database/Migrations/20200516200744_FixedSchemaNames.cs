ae17cafb9fbae2a5c573317e0c69af7c57279c69
Saria Houloubi <houloubis@gmail.com>
2020-05-17 17:42:44 +0400

    Revert "Revert "Aded Db models for working with AWP homework""
    This reverts commit 052f2f462807f8099c6be740c8668917ef922779.

---
+++ SVU/SVU.Database/Migrations/20200516200744_FixedSchemaNames.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SVU.Database.Migrations
+{
+    public partial class FixedSchemaNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HealthUserRoles_HealthRoles_RoleId",
+                schema: "Health",
+                table: "HealthUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HealthUserRoles_Users_UserId",
+                schema: "Health",
+                table: "HealthUserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HealthUserRoles",
+                schema: "Health",
+                table: "HealthUserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HealthRoles",
+                table: "HealthRoles");
+
+            migrationBuilder.RenameTable(
+                name: "HealthUserRoles",
+                schema: "Health",
+                newName: "UserRoles",
+                newSchema: "Health");
+
+            migrationBuilder.RenameTable(
+                name: "HealthRoles",
+                newName: "Roles",
+                newSchema: "Health");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HealthUserRoles_UserId",
+                schema: "Health",
+                table: "UserRoles",
+                newName: "IX_UserRoles_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HealthUserRoles_RoleId",
+                schema: "Health",
+                table: "UserRoles",
+                newName: "IX_UserRoles_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                schema: "Health",
+                table: "UserRoles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                schema: "Health",
+                table: "Roles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                schema: "Health",
+                table: "UserRoles",
+                column: "RoleId",
+                principalSchema: "Health",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                schema: "Health",
+                table: "UserRoles",
+                column: "UserId",
+                principalSchema: "Health",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                schema: "Health",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                schema: "Health",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                schema: "Health",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                schema: "Health",
+                table: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                schema: "Health",
+                newName: "HealthUserRoles",
+                newSchema: "Health");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                schema: "Health",
+                newName: "HealthRoles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_UserId",
+                schema: "Health",
+                table: "HealthUserRoles",
+                newName: "IX_HealthUserRoles_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_RoleId",
+                schema: "Health",
+                table: "HealthUserRoles",
+                newName: "IX_HealthUserRoles_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HealthUserRoles",
+                schema: "Health",
+                table: "HealthUserRoles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HealthRoles",
+                table: "HealthRoles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HealthUserRoles_HealthRoles_RoleId",
+                schema: "Health",
+                table: "HealthUserRoles",
+                column: "RoleId",
+                principalTable: "HealthRoles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HealthUserRoles_Users_UserId",
+                schema: "Health",
+                table: "HealthUserRoles",
+                column: "UserId",
+                principalSchema: "Health",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ SVU/SVU.Database/Models/BlogTag.cs
@@ -0,0 +1,36 @@
+﻿using SVU.Database.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SVU.Database.Models
+{
+    [Table("BlogTags",Schema ="Application")]
+    public class BlogTag : BaseEntityModel
+    {
+        #region Properties
+
+        [ForeignKey(nameof(Blog))]
+        public Guid BlogId{ get; set; }
+        [ForeignKey(nameof(Tag))]
+        public Guid TagId { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        public Blog Blog { get; set; }
+        public Tag Tag{ get; set; }
+        #endregion
+
+
+        #region Constructer
+        /// <summary>
+        /// Default constructer
+        /// </summary>
+        public BlogTag()
+        {
+
+        }
+        #endregion
+    }
+}
