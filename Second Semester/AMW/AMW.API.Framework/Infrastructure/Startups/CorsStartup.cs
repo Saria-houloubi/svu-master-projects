65bb2b710da87b6c780c2b0d3257537bd7f3f0d3
Saria Houloubi <houloubis@gmail.com>
2020-11-21 18:55:10 +0400

    Added Cors in dev settings

---
+++ Second Semester/AMW/AMW.API.Framework/Infrastructure/Startups/CorsStartup.cs
@@ -0,0 +1,52 @@
+﻿using AMW.API.Framework.Abstracion;
+using AMW.API.Framework.Models.Cors;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AMW.API.Framework.Infrastructure.Startups
+{
+    public class CorsStartup : IAmwStartUp
+    {
+        public int Order => 6;
+
+        public void Configure(IApplicationBuilder builder, IHostingEnvironment env, IConfiguration configuration)
+        {
+            var activePolicyName = configuration["Cors:policy:active"];
+
+            builder.UseCors(activePolicyName);
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+
+            var corsConfig = new CorsWrapper();
+
+            var activePolicyName = configuration["Cors:policy:active"];
+
+            configuration.GetSection($"Cors:policy:{activePolicyName}").Bind(corsConfig);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(activePolicyName, buider =>
+                {
+                    buider.WithExposedHeaders(corsConfig.ExposedHeaders);
+                    buider.WithOrigins(corsConfig.AllowedOrigins);
+                    buider.WithMethods(corsConfig.AllowedMethods);
+                    buider.WithHeaders(corsConfig.AllowedHeaders);
+                    if (corsConfig.SupportsCredentials)
+                    {
+                        buider.AllowCredentials();
+                    }
+                    else
+                    {
+                        buider.DisallowCredentials();
+                    }
+                });
+            });
+        }
+
+
+    }
+}
---
+++ Second Semester/AMW/AMW.API.Framework/Models/Cors/CorsWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMW.API.Framework.Models.Cors
+{
+    public class CorsWrapper
+    {
+        #region Properties
+        public string[] ExposedHeaders { get; set; }
+        public string[] AllowedOrigins { get; set; }
+        public string[] AllowedMethods { get; set; }
+        public string[] AllowedHeaders { get; set; }
+        public bool SupportsCredentials { get; set; }
+        #endregion
+
+        #region Constructer
+        /// <summary>
+        /// Default constructer
+        /// </summary>
+        public CorsWrapper()
+        {
+
+        }
+        #endregion
+    }
+}
